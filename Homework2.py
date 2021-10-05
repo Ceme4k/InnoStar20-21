164040aa7cb897a810a702950aae9f8be3311d1b
Ceme4k <roma.solovyev@bk.ru>
2021-10-05 20:02:15 +0300

    test commit

--- Homework2.py
+++ Homework2.py
@@ -4,7 +4,6 @@ print(int(ruble_and_penny * pie))
 
 
 ruble = float(input("Введи стоимость пирожка(в рублях) - "))
-penny = int(float(input("Введи стоимость пирожка(в копейках) - ")))
+penny = float(input("Введи стоимость пирожка(в копейках) - "))
 pie = float(input("Введи количество пирожков - "))
-a = penny / 100
 print(int((ruble + (penny / 100)) * pie))
