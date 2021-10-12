c44b4b942f146bce16c22e4ca8a459f9e974d4ac
Ceme4k <roma.solovyev@bk.ru>
2021-10-12 21:26:32 +0300

    Lesson 5

---
+++ Lesson_1.py
@@ -0,0 +1,62 @@
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a >= b and a >= c:
+#     print(a)
+# elif b >= c and b >= a:
+#     print(b)
+# else:
+#     print(c)
+#
+# print(max(int(input()), int(input()), int(input())))
+
+#
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# if a == c or b == d:
+#     print("Yes")
+# else:
+#     print("No")
+#
+#
+#
+# n = int(input())
+# m = int(input())
+# k = int(input())
+#
+# if k % n == 0 or k % m == 0:
+#     print("Yes")
+# elif k <= n * m:
+#     print("Невозможно")
+# else:
+#     print("No")
+
+#
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# # if (a + b) % 2 == 0 or (a + b) % 2 == 1 and (c + b) % 2 == 0 or (a + b) % 2 == 1 and (a + c) % 2 == 0 or (a + b) % 2 == 1:
+# #     print("No")
+# # else:
+# #     print("Yes")
+#
+# if a % 2 == b % 2 == c % 2:
+#     print("No")
+# else:
+#     print("Yes")
+
+
+
+
+# a = int(input())
+# b = int(input())
+# if a == 0 and b == 0:
+#     print("INF")
+# elif a == 0 and (b % a) != 0:
+#     print("No")
+# else:
+#     print("yes")
+#     print(int(-b / a))
+#
