c7e74a4fa31e44791c4fa0faffaa347e93c8975a
Ceme4k <roma.solovyev@bk.ru>
2021-11-02 18:16:01 +0300

    Lessons 8

--- Ex1,2,3.py
+++ Ex1,2,3.py
@@ -1,3 +1,19 @@
-# Упражнение 1
-a = int(input())
-c = 0
+import random
+
+number = int(input("Введи число от 1 до 100 -"))
+count = int(input("За сколько попыток ты хочешь выйграть?"))
+number_2 = random.randint(1, 100)
+while number != number_2:
+    if count <= 0:
+        print("Ты проиграл!")
+        break
+    if number > 100:
+        print("Введи число от 1 до 100\nПожалуйста пжпжпжпжпжпжпж")
+    elif number < number_2:
+        print("Введи число больше!")
+    elif number > number_2:
+        print("Введи число меньше!")
+    number = int(input("-"))
+    count -= 1
+else:
+    print("Ты победил!")
