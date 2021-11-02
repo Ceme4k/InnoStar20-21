0f489173e7be183e9c4f6ddd49616b514375e30d
Ceme4k <roma.solovyev@bk.ru>
2021-11-02 20:22:47 +0300

    Урок 9 Операции со строками

---
+++ Lessons_9.py
@@ -0,0 +1,42 @@
+# a = input()
+# for i in range(len(a)):
+#     print(a[i], end=" ")
+
+# for i in a:
+#     print(i, end=" ")
+
+# a = input()
+# b = int(a[0]) + int(a[1]) + int(a[2]) + int(a[2]) + int(a[2])
+# c = int(a[1])
+# d = int(a[2])
+# x = int(a[3])
+# p = int(a[4])
+# o = int(a[5])
+# if (b + c + d) == (x + p + o):
+#     print("YES")
+# else:
+#     print("NO")
+
+
+# j = 0
+# for i in range(len(a)):
+#     if int(a[i]) % 2 == 0:
+#         j = 1
+# if j == 1:
+#     print("Yes")
+# else:
+#     print("No")
+#
+# input_string = input()
+# for digit in input_string:
+#     if int(digit) % 2 != 0:
+#         print("No")
+#         break
+# else:
+#     print("Yes")
+
+
+a = int(input())
+b = a + 1
+d = chr(b)
+print(d)
