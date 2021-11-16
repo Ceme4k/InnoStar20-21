f9e291e3d5cff0bc0e126225f258fdb341ec08ae
Ceme4k <roma.solovyev@bk.ru>
2021-11-16 20:07:13 +0300

    Spisok Metod

---
+++ List.py
@@ -0,0 +1,21 @@
+# b = list(map(int, input().split()))
+# for i in range(len(b) + 1):
+#     if i % 2 == 0:
+#         print(i)
+
+# a = list(map(int, input().split()))
+# d = []
+# for i in range(0, len(a)):
+#     if i % 2 == 0:
+#         d.append(a[i])
+#         d += [a[i]]
+#
+# print(*d)
+
+
+my_list = [1, 5, "Hello", True, None, -5.24]
+# my_list.remove(None)
+# my_list.pop()
+# my_list.pop(3)
+# my_list.clear()
+# print(*my_list)
