9099792850dc2e4c607723bae3c9e8925cb4bc78
Ceme4k <roma.solovyev@bk.ru>
2021-11-16 18:18:01 +0300

    PCMS2

---
+++ Lessons_for.py
@@ -0,0 +1,20 @@
+# n = int(input())
+# b = 0
+# for i in range(n):
+#     a = int(input())
+#     if i % 2 == 0:
+#         b += a
+#     else:
+#         b -= a
+# print(b)
+
+
+a = "Hello World"
+d = a.upper()
+v = a.lower()
+h = a.count("l")
+print(d)
+print(v)
+print(h)
+print(a.count("l", 3, 5))
+print(a.count("l", 4, 9))
